b6479267d5e1afaa85bea394717a69df8c6e6e52
Alexis Soucaze <Alexis.Soucaze@bastide-medical.fr>
2024-09-23 16:34:59 +0200

    premier mport

---
+++ carre.py
@@ -0,0 +1,10 @@
+taille=int(input("Taille du caré : "))
+print("carré plein : ")
+for n in range(1,taille):   
+    print("*"*taille)  
+print("Carré vide : ") 
+for i in range(1,taille+1):
+    if i ==1 or i==taille:  
+        print("*"*taille)
+    else:
+        print("*","+"*(taille-2),"*")    
